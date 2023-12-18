119dbe2fd7f72d4ea050051d7b31e4d0863c5316
JerryWu <wujerry116@gmail.com>
2023-12-18 07:10:27 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,19 @@
+function skillsMember() {
+    var skills = document.getElementById("skills");
+    var skillsMember = document.getElementById("skillsMember");
+    var skillsMemberValue = skillsMember.value;
+    var skillsMemberText = skillsMember.options[skillsMember.selectedIndex].text;
+    var skillsMemberTextValue = skillsMemberText + " " + skillsMemberValue;
+    var skillsMemberTextValueTrim = skillsMemberTextValue.trim();
+    var skillsMemberTextValueTrimLength = skillsMemberTextValueTrim.length;
+    var skillsMemberTextValueTrimLengthMinus = skillsMemberTextValueTrimLength - 1;
+    var skillsMemberTextValueTrimLengthMinusMinus = skillsMemberTextValueTrimLength - 2;
+    var skillsMemberTextValueTrimLengthMinusMinusMinus = skillsMemberTextValueTrimLength - 3;
+    var skillsMemberTextValueTrimLengthMinusMinusMinusMinus = skillsMemberTextValueTrimLength - 4;
+    var skillsMemberTextValueTrimLengthMinusMinusMinusMinusMinus = skillsMemberTextValueTrimLength - 5;
+    var skillsMemberTextValueTrimLengthMinusMinusMinusMinusMinusMinus = skillsMemberTextValueTrimLength - 6;
+    var skillsMemberTextValueTrimLengthMinusMinusMinusMinusMinusMinusMinus = skillsMemberTextValueTrimLength - 7;
+    var skillsMemberTextValueTrimLengthMinusMinusMinusMinusMinusMinusMinusMinus = skillsMemberTextValueTrimLength - 8;
+    var skillsMemberTextValueTrimLengthMinusMinusMinusMinusMinusMinusMinusMinusMinus = skillsMemberTextValueTrimLength - 9;
+    var skillsMemberTextValueTrimLengthMinusMinusMinusMinusMinusMinusMinusMinusMinusMinus = skillsMemberTextValueTrimLength - 10;
+}
